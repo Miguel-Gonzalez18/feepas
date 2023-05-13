c4ccf2f599c75676f19f2f4389a2ab3742cf6917
Miguel Ángel González <axtra2766@gmail.com>
2023-05-12 20:47:03 -0400

    Libreria de animaciones AOS agregadas, Para darle animaciones de entradas a algunos elmentos de la página

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,11 @@ import Footer from '@/components/footer'
 
 import { usePathname } from 'next/navigation';
 
+//Animaciones
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 export const lobster = Lobster({
   weight: ['400'],
